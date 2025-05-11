b7fd36399134dcb209d31a3e2f78ab6143f88d8a
joaoVitorLeal <joaoleal98@outlook.com>
2025-05-11 18:52:56 -0300

    feat: add the component search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from "@angular/material/button";
+import { ClientService } from '../client.service';
+import { Client } from '../register/client';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    FormsModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+
+  clientList: Client[] = [];
+
+  constructor(private clientService: ClientService) { }
+
+  ngOnInit(): void {
+    this.clientList = this.clientService.search('');
+  }
+
 
 }
