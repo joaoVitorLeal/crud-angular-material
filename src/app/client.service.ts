36299699ae0025c03de9b76855f792044437d435
joaoVitorLeal <joaoleal98@outlook.com>
2025-05-11 18:54:54 -0300

    feat: add client service

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -6,9 +6,30 @@ import { Client } from './register/client';
 })
 export class ClientService {
 
+  static readonly CLIENT_REPOSITORY_KEY = "CLIENTS";
+
   constructor() { }
 
-  save(client: Client) {
-    console.log(client);
+  save(client: Client): void {
+    const storage = this.getStorage();
+    storage.push(client);
+
+    localStorage.setItem(ClientService.CLIENT_REPOSITORY_KEY, JSON.stringify(storage));
+  }
+
+  search(name: string): Client[] {
+    return this.getStorage();
+  }
+
+  private getStorage(): Client[] {
+    const clientRepository = localStorage.getItem(ClientService.CLIENT_REPOSITORY_KEY);
+    if(clientRepository) {
+      const clients: Client[] = JSON.parse(clientRepository);
+      return clients;
+    }
+
+    const clients: Client[] = [];
+    localStorage.setItem(ClientService.CLIENT_REPOSITORY_KEY, JSON.stringify(clients));
+    return clients;
   }
 }
