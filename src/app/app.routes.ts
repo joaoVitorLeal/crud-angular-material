64520d4d13dfd8f697757f82f988c3e986025cac
joaoVitorLeal <joaoleal98@outlook.com>
2025-05-10 22:20:05 -0300

    chore: re-add project files, ignore unnecessary ones

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { SearchComponent } from './search/search.component';
-
-export const routes: Routes = [
-    { path: 'register', component: RegisterComponent },
-    { path: 'search', component: SearchComponent }
-];
