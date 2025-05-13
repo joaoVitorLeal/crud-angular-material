b2e6ce2b96ba9e29badc3211b1bf2ccd817a85bb
joaoVitorLeal <joaoleal98@outlook.com>
2025-05-12 22:49:48 -0300

    Update register component to load states and cities from BrazilAPI

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -7,11 +7,14 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar'
+import { MatSelectChange, MatSelectModule } from '@angular/material/select'
 import { Client } from './client';
 import { ClientService } from '../client.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask'
-
+import { BrazilApiService } from '../brazil-api.service';
+import { BrazilianState, City } from '../brasil-api.models';
+import { CommonModule } from '@angular/common';
  
 @Component({
   selector: 'app-register',
@@ -23,7 +26,9 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask'
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    NgxMaskDirective
+    MatSelectModule,
+    NgxMaskDirective,
+    CommonModule
   ], providers: [
     provideNgxMask()
   ],
@@ -35,13 +40,15 @@ export class RegisterComponent implements OnInit {
   client: Client = Client.newClient();
   isUpdating: boolean = false;
   private _snackBar: MatSnackBar = inject(MatSnackBar);
+  states: BrazilianState[] = [];
+  cities: City[] = [];
 
   constructor(
     private clientService: ClientService,
+    private brazilApiService: BrazilApiService,
     private route: ActivatedRoute,
     private router: Router
-  ) { // injeção de dependência por @Injectable 
-  }
+  ) { } // injeção de dependência por @Injectable
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe( (query: any) => {
@@ -53,11 +60,38 @@ export class RegisterComponent implements OnInit {
         if (foundClient) {
           this.isUpdating = true;
           this.client = foundClient;
+
+          if (this.client.stateAbbreviation) {
+            const event = {value: this.client.stateAbbreviation}
+            this.loadCities(event as MatSelectChange)
+          }
         }
       }
     });
+
+    this.loadUfs();
   }
   
+  loadUfs(): void {
+    console.log("Carregando UFs....");
+    
+    // Observable - Subscribe
+      this.brazilApiService.listUfs().subscribe({
+      next: stateList => this.states = stateList, // Sucesso – Retorna uma lista de estados
+      error: erro => console.log("Ocorreu um erro ao listar cidades: ", erro) // Erro – retorna um erro declaro pela API
+    });
+  }
+
+  loadCities(event: MatSelectChange): void {
+    const selectedUf = event.value;
+    // Observable - Subscribe
+    this.brazilApiService.listCities(selectedUf).subscribe({
+      next: citiesList => this.cities = citiesList,
+      error: erro => console.log('Ocorreu um erro ao listar municípios: ' + erro)
+      
+    });
+  }
+
   save(): void {
     if (!this.isUpdating) {
       this.clientService.save(this.client);
