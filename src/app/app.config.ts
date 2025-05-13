581614375a071fffffde893166d07e0c9d22f7e6
joaoVitorLeal <joaoleal98@outlook.com>
2025-05-12 22:53:16 -0300

    chore(config): register BrazilAPI service for states and cities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http'
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()) // Consumir APIs externas
+  ] // providers: Responsáveis por disponibilizar serviços, configurações ou funções através da injeção de dependência do Angular
 };
